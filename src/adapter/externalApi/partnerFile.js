c5f36786d5190e1dde264e2955307aeb68fd1d25
Felipe Marchioli <fmarchioli@iouu.com.br>
2021-06-17 02:48:19 -0300

    feat: partner api integration and then follow as business rule

---
+++ src/adapter/externalApi/partnerFile.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const parser = require('xml2json');
+const { response } = require('../../app');
+
+const fileData = require('../../domain/fileData');
+
+dotenv.config();
+
+exports.getPartnerFile = async () => {
+  const options = { headers: { 'Content-Type': 'application/json',
+                               'X-Api-Id': process.env.PARTNER_API_ID,
+                               'X-Api-Key': process.env.PARTNER_API_KEY
+                              }
+                            };
+  await axios.get(process.env.PARTNER_URL, options)
+    .then((res) => {
+      // Receive api data
+      const files = res.data.data;
+      for (file of files) {
+        const accessKey = `{${file['access_key']}}`;
+    
+        // Transform xml file to JSON file
+        fs.writeFileSync('./example.xml', (Buffer.from(file.xml, 'base64')));
+
+        fs.readFile('./example.xml', async (err, data) => {
+          const file = JSON.parse(parser.toJson(data, {reversible: true}));
+          const value = parseFloat(file.nfeProc.NFe.infNFe.pag.detPag.vPag['$t']);
+
+          // Send to save on DB
+          const response = await fileData.saveFile(accessKey, value);
+          if (response || err) {
+            return response;
+          }
+        });
+      };
+
+      return null;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
--- src/route/fileRoute.js
+++ src/route/fileRoute.js
@@ -14,4 +14,13 @@ FileRoute.get('/file/value', async (req, res) => {
   return res.status(200).json({ message: 'Success', value: fileData.value})
 });
 
+FileRoute.post('/file/partner', async (_, res) => {
+  const response = await fileService.saveFiles();
+  if (response) {
+    return res.status(400).json({ message: 'Error', value: 'Error to try get and save files from partner'})
+  }
+
+  return res.status(201).json({ message: 'Success', value: 'All files were created'})
+});
+
 module.exports = FileRoute;
